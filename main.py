ac0506b6e14d71671ad007cf9698b4890787dea9
klausmenendez <menendek@oregonstate.edu>
2022-10-18 09:12:49 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def fall_distance(seconds):
+    distance=0.5*9.8*seconds**2
+    return distance
+
+# print(fall_distance(3.5))
